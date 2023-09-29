4397784ed0473dbcaa16590d3d24c16b3db95f7c
yanstudy <evr.studying@gmail.com>
2023-09-29 16:32:27 +0300

    unknown

--- app.js
+++ app.js
@@ -35,7 +35,7 @@ app.post('/signup', celebrate({
   body: Joi.object().keys({
     email: Joi.string().required().min(2).max(30),
     password: Joi.string().required().min(2),
-  }),
+  }).unknown(true),
 }), createUser);
 
 // Авторизация
