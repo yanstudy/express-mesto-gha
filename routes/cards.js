b755b99ffa85dd2c9dabeca5283640aa8ac2af2b
yanstudy <evr.studying@gmail.com>
2023-10-02 16:50:27 +0300

    params

--- routes/cards.js
+++ routes/cards.js
@@ -9,6 +9,12 @@ const {
   deleteLike,
 } = require('../controllers/cards');
 
+const checkIdCards = celebrate({
+  params: Joi.object().keys({
+    cardId: Joi.string().alphanum().length(24),
+  }),
+});
+
 router.get('/cards', getCards);
 router.post('/cards', celebrate({
   body: Joi.object().keys({
@@ -16,17 +22,10 @@ router.post('/cards', celebrate({
     link: Joi.string().required().regex(/^(https?:\/\/)[a-zA-Z0-9-._~:/?#[\]@!$&'()*+,;=]/),
   }).unknown(true),
 }), createCard);
-router.delete('/cards/:cardId', deleteCardById);
-router.put('/cards/:cardId/likes', celebrate({
-  params: Joi.object().keys({
-    cardId: Joi.string().alphanum().length(24),
-  }),
-}), addLike);
-router.delete('/cards/:cardId/likes', celebrate({
-  params: Joi.object().keys({
-    cardId: Joi.string().alphanum().length(24),
-  }),
-}), deleteLike);
+
+router.delete('/cards/:cardId', checkIdCards, deleteCardById);
+router.put('/cards/:cardId/likes', checkIdCards, addLike);
+router.delete('/cards/:cardId/likes', checkIdCards, deleteLike);
 
 router.use(errors());
 module.exports = router;
